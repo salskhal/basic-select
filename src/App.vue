3d6e8ea1115d4462ef15b417bec608cad13d4843
salskhal <olaniyisal@gmail.com>
2022-04-25 16:47:55 +0100

    Demo/ Initial commit of the demo file

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,48 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+
+  <h1>Hello world</h1>
+  <form @submit.prevent="search">
+  <input type="text" v-model="film" list="availableBreeds" />
+  </form>
+  <datalist id="availableBreeds">
+    <option v-for="breed in availableBreeds" :key="breed.id">{{ breed.name }}</option>
+  </datalist>
+  <h1>{{film}}</h1>
 </template>
+
+<script setup>
+import {ref, onMounted} from "vue"
+
+const availableBreeds = ref([]);
+const film = ref("")
+
+const API_KEY = "7c306308-15a5-45c8-9f7f-d215f10ddf53";
+
+
+
+onMounted(() => {
+    fetch(`https://api.thecatapi.com/v1/breeds?api_key=${API_KEY}`)
+    .then((res) => res.json())
+    .then((breeds) => {
+      console.log(breeds)
+      availableBreeds.value = breeds
+      
+    });
+})
+
+
+const search = () =>{
+  
+} 
+
+// const fetchBreads = () => {
+//   fetch(`https://api.thecatapi.com/v1/breeds?api_key=${API_KEY}`)
+//     .then((res) => res.json())
+//     .then((breeds) => {
+//       availableBreeds.value = breeds.map(breed => breed.name)
+//     });
+// };
+</script>
 
 <style>
 #app {
